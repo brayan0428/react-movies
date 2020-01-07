dc392cbd75849b1d7115c6623f43cd45a95556d6
Brayan Llanos <brayan0428@gmail.com>
2020-01-06 21:33:50 -0500

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { Title } from "./components/Title";
+import "bulma/css/bulma.css";
+import SearchForm from "./components/SearchForm";
+import { MovieList } from "./components/MovieList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = { movies: [] };
+
+  llenarPeliculas = results => {
+    this.setState({ movies: results });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Title>Buscador de Peliculas</Title>
+        <div className="SearchForm-wrapper">
+          <SearchForm onResults={this.llenarPeliculas} />
+        </div>
+        {this.state.movies.length === 0 ? (
+          <p>Sin resultados</p>
+        ) : (
+          <MovieList movies={this.state.movies} />
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/SearchForm.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+
+const API_KEY = "57cf211b";
+
+class SearchForm extends Component {
+  state = { inputTitle: "" };
+
+  sendForm = e => {
+    e.preventDefault();
+    const { inputTitle } = this.state;
+    fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${inputTitle}`)
+      .then(result => result.json())
+      .then(data => {
+        const { Search } = data;
+        this.props.onResults(Search);
+      });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.sendForm}>
+        <div className="field has-addons">
+          <div className="control">
+            <input
+              value={this.state.title}
+              className="input"
+              type="text"
+              placeholder="Ingrese la pelicula..."
+              onChange={e => this.setState({ inputTitle: e.target.value })}
+            />
+          </div>
+          <div className="control">
+            <button className="button is-info">Buscar</button>
+          </div>
+        </div>
+      </form>
+    );
+  }
+}
+
+export default SearchForm;
